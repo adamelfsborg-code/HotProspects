4558a43de761b1b3f988470b74c1e923403c8c5b
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-19 15:22:37 +0200

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Adam Elfsborg on 2024-08-19.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
