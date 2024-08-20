3128c9829b3f361e40551dc0149c09605a336bbc
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-20 17:24:04 +0200

    feat: result type and tab view

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,24 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedTab = "One"
+    @State private var output = ""
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Button("Show tab two") {
-                selectedTab = "Two"
+        Text(output)
+            .task {
+                await fetchReadings()
             }
-            .tag("One")
-            .tabItem {
-                Label("One", systemImage: "rectangle")
-            }
-            Button("Show tab one") {
-                selectedTab = "One"
-            }
-            .tag("Two")
-            .tabItem {
-                Label("Two", systemImage: "circle")
-            }
+    }
+    
+    func fetchReadings() async {
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+        
+        let result = await fetchTask.result
+        
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let err):
+            output = "Error \(err.localizedDescription)"
         }
     }
 }
