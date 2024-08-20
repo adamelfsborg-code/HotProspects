805f192ec3f99aa12c05e720485432c261867262
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-20 17:44:43 +0200

    feat: context menu

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State private var backgroundColor = Color.red
     var body: some View {
-        Image(.example)
-            .interpolation(.none)
-            .resizable()
-            .scaledToFit()
-            .background(.black)
+        VStack {
+            
+            Rectangle()
+                .fill(backgroundColor)
+                .cornerRadius(20)
+                .frame(height: 300)
+                .padding()
+                .cornerRadius(20)
+            
+           
+            Text("Change Background")
+                .padding()
+                .contextMenu {
+                    Button("Red",systemImage: backgroundColor == .red ? "checkmark.circle.fill" : "checkmark.circle", role: .destructive) {
+                        withAnimation {
+                            backgroundColor = .red
+                        }
+                    }
+                    Button("Blue", systemImage: backgroundColor == .blue ? "checkmark.circle.fill" : "checkmark.circle") {
+                        withAnimation {
+                            backgroundColor = .blue
+                        }
+                    }
+                    Button("Yellow", systemImage: backgroundColor == .yellow ? "checkmark.circle.fill" : "checkmark.circle") {
+                        withAnimation {
+                            backgroundColor = .yellow
+                        }
+                    }
+                    Button("Green", systemImage: backgroundColor == .green ? "checkmark.circle.fill" : "checkmark.circle") {
+                        withAnimation {
+                            backgroundColor = .green
+                        }
+                    }
+                    Button("Cancel", systemImage: "checkmark.circle") {}
+                }
+        }
     }
     
 
