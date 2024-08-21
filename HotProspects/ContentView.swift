79be39e4c27f36133c454ed37522c860375568b8
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-08-21 07:13:58 +0200

    feat: swipe actions

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,42 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
     var body: some View {
-        VStack {
-            
-            Rectangle()
-                .fill(backgroundColor)
-                .cornerRadius(20)
-                .frame(height: 300)
-                .padding()
-                .cornerRadius(20)
-            
-           
-            Text("Change Background")
-                .padding()
-                .contextMenu {
-                    Button("Red",systemImage: backgroundColor == .red ? "checkmark.circle.fill" : "checkmark.circle", role: .destructive) {
-                        withAnimation {
-                            backgroundColor = .red
-                        }
+        List {
+            Text("Steve Jobs")
+                .swipeActions {
+                    Button("Delte", systemImage: "minus.circle", role: .destructive) {
+                        print("Delete")
                     }
-                    Button("Blue", systemImage: backgroundColor == .blue ? "checkmark.circle.fill" : "checkmark.circle") {
-                        withAnimation {
-                            backgroundColor = .blue
-                        }
+                }
+                .swipeActions(edge: .leading) {
+                    Button("Message", systemImage: "message") {
+                        print("Hi")
                     }
-                    Button("Yellow", systemImage: backgroundColor == .yellow ? "checkmark.circle.fill" : "checkmark.circle") {
-                        withAnimation {
-                            backgroundColor = .yellow
-                        }
+                    Button("Pin", systemImage: "pin") {
+                        print("Pinned")
                     }
-                    Button("Green", systemImage: backgroundColor == .green ? "checkmark.circle.fill" : "checkmark.circle") {
-                        withAnimation {
-                            backgroundColor = .green
-                        }
-                    }
-                    Button("Cancel", systemImage: "checkmark.circle") {}
+                    .tint(.orange)
                 }
         }
     }
